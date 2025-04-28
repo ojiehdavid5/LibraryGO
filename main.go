94e40d0d9dd53cfc72ac0162d050df46987a5517
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-28 15:50:56 +0100

    finally done

--- main.go
+++ main.go
@@ -1,36 +1,38 @@
 package main
+
 import (
-	"fmt"
 	"database/sql"
-	_ "github.com/lib/pq"
+	"fmt"
 	"log"
+
 	"github.com/gofiber/fiber/v2"
-
-
-
+	_ "github.com/lib/pq"
 )
 
 type Book struct {
-	Title  string `json:"title"`
 	Author string `json:"author"`
-		Publisher   int    `json:"publisher"`}
+
+	Title  string `json:"title"`
+		Publisher   string	  `json:"publisher"`}
 
 func main(){
 
 
 
-	connStr := "postgresql://user:library@localhost:5432/postgres?options"
+	connStr := "postgresql://user:library@localhost:5432/postgres?sslmode=disable"
 
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error connecting to the database: ", err)
 	}
-	defer db.Close()
+	// defer db.Close()
 
-	fmt.Println("hello")
+	fmt.Println("connected to the database")
 
 	app:= fiber.New()
+
+
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return getHandler(c,db);
@@ -39,9 +41,11 @@ func main(){
 		return postHandler(c,db);
 	})
 
-	app.Delete("/", func(c *fiber.Ctx) error {
+	app.Delete("/del", func(c *fiber.Ctx) error {
 		return deleteHandler(c,db);
 	})
+	app.Listen(":8080")
+
 
 
 }
@@ -49,12 +53,53 @@ func main(){
 
 
 func getHandler(c *fiber.Ctx, db *sql.DB) error {
+	rows, err := db.Query("SELECT author, title FROM books")	
+	if err != nil {
+		return c.Status(500).SendString("Error querying the database")
+	}
+	defer rows.Close()
+
+	var books []Book
+	for rows.Next() {
+		var book Book
+		if err := rows.Scan(&book.Title, &book.Author); err != nil {
+			return c.Status(500).SendString("Error scanning row")
+		}
+		books = append(books, book)
+	}
+
+	return c.JSON(books)
 
 }
 
 func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	var book Book
+	if err := c.BodyParser(&book); err != nil {
+		return c.Status(400).SendString("Invalid request body")
+	}
+	fmt.Println(book)
+	_, err := db.Exec("INSERT INTO books ( author, title, publisher) VALUES ($1, $2, $3)", book.Author,book.Title, book.Publisher)
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(500).SendString("Error inserting into the database")
+	}
+	c.SendString("Book added successfully")	
+
+	return c.Redirect("/")
 
 }
 func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	var book Book
+	if err := c.BodyParser(&book); err != nil {
+		return c.Status(400).SendString("Invalid request body")
+	}
+
+	_, err := db.Exec("DELETE FROM books WHERE title = $1", book.Title)
+	if err != nil {
+		return c.Status(500).SendString("Error deleting from the database")
+	}
+
+	return c.Redirect("/")
 	
+
 }
