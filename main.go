6c68dc3146bc74c806773fb8ff476b11ef79fb6b
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-29 10:23:23 +0100

    format done

--- main.go
+++ main.go
@@ -12,48 +12,40 @@ import (
 type Book struct {
 	Author string `json:"author"`
 
-	Title  string `json:"title"`
-		Publisher   string	  `json:"publisher"`}
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
 
-func main(){
-
-
+func main() {
 
 	connStr := "postgresql://user:library@localhost:5432/postgres?sslmode=disable"
-
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error connecting to the database: ", err)
 	}
-	// defer db.Close()
+	defer db.Close()
 
 	fmt.Println("connected to the database")
 
-	app:= fiber.New()
-
-
+	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return getHandler(c,db);
+		return getHandler(c, db)
 	})
 	app.Post("/", func(c *fiber.Ctx) error {
-		return postHandler(c,db);
+		return postHandler(c, db)
 	})
 
-	app.Delete("/del", func(c *fiber.Ctx) error {
-		return deleteHandler(c,db);
+	app.Delete("/", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
 	})
 	app.Listen(":8080")
 
-
-
 }
 
-
-
 func getHandler(c *fiber.Ctx, db *sql.DB) error {
-	rows, err := db.Query("SELECT author, title FROM books")	
+	rows, err := db.Query("SELECT author, title FROM books")
 	if err != nil {
 		return c.Status(500).SendString("Error querying the database")
 	}
@@ -78,14 +70,14 @@ func postHandler(c *fiber.Ctx, db *sql.DB) error {
 		return c.Status(400).SendString("Invalid request body")
 	}
 	fmt.Println(book)
-	_, err := db.Exec("INSERT INTO books ( author, title, publisher) VALUES ($1, $2, $3)", book.Author,book.Title, book.Publisher)
+	_, err := db.Exec("INSERT INTO books ( author, title, publisher) VALUES ($1, $2, $3)", book.Author, book.Title, book.Publisher)
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(500).SendString("Error inserting into the database")
 	}
-	c.SendString("Book added successfully")	
+	c.SendString("Book added successfully")
 
-	return c.Redirect("/")
+	return c.SendStatus(201)
 
 }
 func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
@@ -99,7 +91,6 @@ func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 		return c.Status(500).SendString("Error deleting from the database")
 	}
 
-	return c.Redirect("/")
-	
+	return c.SendStatus(204)
 
 }
